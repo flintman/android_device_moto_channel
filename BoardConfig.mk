LineageOS/android_device_motorola_channel

channel: Make vendor board-configuration mandatory
Change-Id: I2cb9aba5f858c845399aa80afdbbf97edfd801dd

BoardConfig.mk
@@ -46,4 +46,4 @@ BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
 # inherit from the proprietary version
--include vendor/motorola/channel/BoardConfigVendor.mk
+include vendor/motorola/channel/BoardConfigVendor.mk

flintman/android_device_moto_channel

BoardConfig.mk
@@ -46,4 +46,4 @@ BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
 # inherit from the proprietary version
--include vendor/moto/channel/BoardConfigVendor.mk
+include vendor/moto/channel/BoardConfigVendor.mk